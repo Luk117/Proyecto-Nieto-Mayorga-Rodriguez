d15fdd7b20a7347ee71f6b97667697db381111c0
NataliaMayorga04 <nataliamapr@unisabana.edu.co>
2024-05-17 09:54:22 -0500

    Cambio eclat

--- recommender.py
+++ recommender.py
@@ -3,27 +3,32 @@ class Recommender:
         self.RULES = []
         self.frequent_itemsets = None
 
-    def eclat(self, transactions, minsup, max_itemset_size=3):
-        def eclat_rec(P, minsup, F, depth=1):
-            if depth > max_itemset_size:
-                return
-            for V, (Xa, tXa) in enumerate(P):
-                if len(tXa) >= minsup:
-                    F.append((Xa, len(tXa)))
-                    Pa = []
-                    for Xb, tXb in P[V+1:]:
-                        I = tXa & tXb
-                        if len(I) >= minsup:
-                            new_set = Xa | Xb
-                            if len(new_set) <= max_itemset_size:
-                                Pa.append((new_set, I))
-                    eclat_rec(Pa, minsup, F, depth + 1)
+    def eclat(self, transactions, minsup):
+        item_tidsets = {}
+        for tid, transaction in enumerate(transactions):
+            for item in transaction:
+                if item in item_tidsets:
+                    item_tidsets[item].add(tid)
+                else:
+                    item_tidsets[item] = {tid}
 
-        item_sets = [(frozenset([item]), set(idx for idx, transaction in enumerate(transactions) if item in transaction))
-                     for item in set().union(*transactions)]
-        F = []
-        eclat_rec(item_sets, minsup, F)
-        return F
+        item_tidsets = {item: tids for item, tids in item_tidsets.items() if len(tids) >= minsup}
+
+        def eclat_recursive(prefix, items_tidsets, frequent_itemsets):
+            sorted_items = sorted(items_tidsets.items())
+            for i, (item, tidset_i) in enumerate(sorted_items):
+                new_itemset = prefix + (item,)
+                frequent_itemsets.append((new_itemset, len(tidset_i)))
+                suffix_tidsets = {}
+                for item_j, tidset_j in sorted_items[i + 1:]:
+                    new_tidset = tidset_i & tidset_j
+                    if len(new_tidset) >= minsup:
+                        suffix_tidsets[item_j] = new_tidset
+                eclat_recursive(new_itemset, suffix_tidsets, frequent_itemsets)
+
+        frequent_itemsets = []
+        eclat_recursive(tuple(), item_tidsets, frequent_itemsets)
+        return frequent_itemsets
 
     def createAssociationRules(self, F, minconf):
         B = []
@@ -31,16 +36,16 @@ class Recommender:
             if len(itemset) > 1:
                 for item in itemset:
                     antecedent = frozenset([item])
-                    consequent = itemset - antecedent
-                    antecedent_support = next((sup for iset, sup in F if iset == antecedent), 0)
+                    consequent = frozenset(itemset) - antecedent
+                    antecedent_support = next((sup for iset, sup in F if frozenset(iset) == antecedent), 0)
                     if antecedent_support > 0:
                         conf = support / antecedent_support
                         if conf >= minconf:
                             B.append((antecedent, consequent, support, conf))
         return B
 
-    def train(self, database, minsup=0.05, minconf=0.7, max_itemset_size=3):
-        self.frequent_itemsets = self.eclat(database, minsup, max_itemset_size)
+    def train(self, database, minsup=0.05, minconf=0.7):
+        self.frequent_itemsets = self.eclat(database, minsup)
         self.RULES = self.createAssociationRules(self.frequent_itemsets, minconf)
         return self
 
@@ -53,3 +58,5 @@ class Recommender:
                         recommendations[item] = recommendations.get(item, 0) + rule[2]
         sorted_recommendations = sorted(recommendations.items(), key=lambda x: x[1], reverse=True)
         return [item for item, _ in sorted_recommendations[:max_recommendations]]
+
+
