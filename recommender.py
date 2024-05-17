f1efddca523551100c7c73890500287e377b0238
NataliaMayorga04 <nataliamapr@unisabana.edu.co>
2024-05-17 00:11:29 -0500

    Test1

--- recommender.py
+++ recommender.py
@@ -1,74 +1,60 @@
 class Recommender:
     def __init__(self):
-        self.RULES = [] 
+        self.RULES = []
         self.frequent_itemsets = None
 
     def eclat(self, transactions, minsup):
-        def eclat_rec(P, minsup, F):
+        def eclat_rec(P, minsup, F, depth=1, max_depth=3):
+            if depth > max_depth:
+                return
             for V, (Xa, tXa) in enumerate(P):
-                F.append((Xa, len(tXa)))
-                Pa = []
-                for Xb, tXb in P[V+1:]:
-                    I = tXa & tXb
-                    if len(I) >= minsup:
-                        Pa.append((sorted(list(set(Xa + Xb))), I))
-                if Pa:
-                    eclat_rec(Pa, minsup, F)
-            return F
+                if len(tXa) >= minsup:
+                    F.append((Xa, len(tXa)))
+                    Pa = []
+                    for Xb, tXb in P[V+1:]:
+                        I = tXa & tXb
+                        if len(I) >= minsup:
+                            new_set = Xa | Xb
+                            if len(new_set) <= max_depth:
+                                Pa.append((new_set, I))
+                    eclat_rec(Pa, minsup, F, depth + 1, max_depth)
 
+        item_sets = [(frozenset([item]), set(idx for idx, transaction in enumerate(transactions) if item in transaction))
+                     for item in set().union(*transactions)]
         F = []
-        item_sets = [(frozenset([item]), set([idx for idx, transaction in enumerate(transactions) if item in transaction])) for item in set(item for sublist in transactions for item in sublist)]
-        return eclat_rec(item_sets, minsup, F)
+        eclat_rec(item_sets, minsup, F)
+        return F
 
-    def createAssociationRules(self, F, minconf, max_set_size=3):
-        def powerset(s):
-            result = [[]]
-            for elem in s:
-                result += [x + [elem] for x in result]
-            return result[1:-1]  # exclude empty set and full set
+    def createAssociationRules(self, F, minconf):
+        def generate_rules_from_itemset(itemset, itemset_support):
+            for item in itemset:
+                antecedent = frozenset([item])
+                consequent = itemset - antecedent
+                antecedent_support = next((sup for iset, sup in F if iset == antecedent), 0)
+                if antecedent_support > 0:
+                    conf = itemset_support / antecedent_support
+                    if conf >= minconf:
+                        yield (antecedent, consequent, itemset_support, conf)
 
         B = []
-        for Z, supZ in [fEntry for fEntry in F if 1 < len(fEntry[0]) <= max_set_size]:
-            A = sorted(powerset(list(Z)), key=lambda l: len(l), reverse=True)
-            while A:
-                X = A.pop(0)
-                O = [xEntry for xEntry in F if xEntry[0] == frozenset(X)][0][1]
-                c = supZ / O
-                if c >= minconf:
-                    B.append((X, list(Z - set(X)), supZ, c))
+        for itemset, support in F:
+            if 1 < len(itemset) <= 3:
+                B.extend(generate_rules_from_itemset(itemset, support))
         return B
 
-    """
-        This is the class to make recommendations.
-        The class must not require any mandatory arguments for initialization.
-    """
-    def train(self, prices, database, minsup=0.5, minconf=0.7) -> None:
-
+    def train(self, prices, database, minsup=0.66, minconf=0.3) -> None:
         self.frequent_itemsets = self.eclat(database, minsup)
         self.RULES = self.createAssociationRules(self.frequent_itemsets, minconf)
-
-        """
-            allows the recommender to learn which items exist, which prices they have, and which items have been purchased together in the past
-            :param prices: a list of prices in USD for the items (the item ids are from 0 to the length of this list - 1)
-            :param database: a list of lists of item ids that have been purchased together. Every entry corresponds to one transaction
-            :return: the object should return itself here (this is actually important!)
-        """
         return self
 
-    def get_recommendations(self, cart:list, max_recommendations:int) -> list:
-        """
-            makes a recommendation to a specific user
-            
-            :param cart: a list with the items in the cart
-            :param max_recommendations: maximum number of items that may be recommended
-            :return: list of at most `max_recommendations` items to be recommended
-        """
-        recommendations = []
+    def get_recommendations(self, cart, max_recommendations):
+        recommendations = {}
         for rule in self.RULES:
-            if set(rule[0]).issubset(set(cart)):
-             recommendations.extend(rule[1])
-            if len(recommendations) >= max_recommendations:
-                break
-            return recommendations[:max_recommendations]
-    
+            if rule[0].issubset(cart):
+                for item in rule[1]:
+                    if item not in cart:
+                        recommendations[item] = recommendations.get(item, 0) + rule[2]
+        sorted_recommendations = sorted(recommendations.items(), key=lambda x: x[1], reverse=True)
+        return [item for item, _ in sorted_recommendations[:max_recommendations]]
+
      
