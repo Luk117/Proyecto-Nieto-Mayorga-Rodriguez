c364151844fee9d549796b017a0bc1227c726158
NataliaMayorga04 <nataliamapr@unisabana.edu.co>
2024-05-17 00:25:12 -0500

    Optimizar

--- recommender.py
+++ recommender.py
@@ -3,9 +3,9 @@ class Recommender:
         self.RULES = []
         self.frequent_itemsets = None
 
-    def eclat(self, transactions, minsup):
-        def eclat_rec(P, minsup, F, depth=1, max_depth=3):
-            if depth > max_depth:
+    def eclat(self, transactions, minsup, max_itemset_size=3):
+        def eclat_rec(P, minsup, F, depth=1):
+            if depth > max_itemset_size:
                 return
             for V, (Xa, tXa) in enumerate(P):
                 if len(tXa) >= minsup:
@@ -15,9 +15,9 @@ class Recommender:
                         I = tXa & tXb
                         if len(I) >= minsup:
                             new_set = Xa | Xb
-                            if len(new_set) <= max_depth:
+                            if len(new_set) <= max_itemset_size:
                                 Pa.append((new_set, I))
-                    eclat_rec(Pa, minsup, F, depth + 1, max_depth)
+                    eclat_rec(Pa, minsup, F, depth + 1)
 
         item_sets = [(frozenset([item]), set(idx for idx, transaction in enumerate(transactions) if item in transaction))
                      for item in set().union(*transactions)]
@@ -26,28 +26,25 @@ class Recommender:
         return F
 
     def createAssociationRules(self, F, minconf):
-        def generate_rules_from_itemset(itemset, itemset_support):
-            for item in itemset:
-                antecedent = frozenset([item])
-                consequent = itemset - antecedent
-                antecedent_support = next((sup for iset, sup in F if iset == antecedent), 0)
-                if antecedent_support > 0:
-                    conf = itemset_support / antecedent_support
-                    if conf >= minconf:
-                        yield (antecedent, consequent, itemset_support, conf)
-
         B = []
         for itemset, support in F:
-            if 1 < len(itemset) <= 3:
-                B.extend(generate_rules_from_itemset(itemset, support))
+            if len(itemset) > 1:
+                for item in itemset:
+                    antecedent = frozenset([item])
+                    consequent = itemset - antecedent
+                    antecedent_support = next((sup for iset, sup in F if iset == antecedent), 0)
+                    if antecedent_support > 0:
+                        conf = support / antecedent_support
+                        if conf >= minconf:
+                            B.append((antecedent, consequent, support, conf))
         return B
 
-    def train(self, prices, database, minsup=0.66, minconf=0.3) -> None:
-        self.frequent_itemsets = self.eclat(database, minsup)
+    def train(self, database, minsup=0.05, minconf=0.7, max_itemset_size=3):
+        self.frequent_itemsets = self.eclat(database, minsup, max_itemset_size)
         self.RULES = self.createAssociationRules(self.frequent_itemsets, minconf)
         return self
 
-    def get_recommendations(self, cart, max_recommendations):
+    def get_recommendations(self, cart, max_recommendations=5):
         recommendations = {}
         for rule in self.RULES:
             if rule[0].issubset(cart):
@@ -56,5 +53,3 @@ class Recommender:
                         recommendations[item] = recommendations.get(item, 0) + rule[2]
         sorted_recommendations = sorted(recommendations.items(), key=lambda x: x[1], reverse=True)
         return [item for item, _ in sorted_recommendations[:max_recommendations]]
-
-     
