e63afda45376a029868fe0f5e829da20fbacd5fe
Luk117 <lukasnieto117@gmail.com>
2024-05-17 11:41:16 -0500

    test of mettrics

--- recommender.py
+++ recommender.py
@@ -1,52 +1,5 @@
 import numpy as np
 from collections import defaultdict
-import itertools
-
-def calculate_supports(D, X, Y=None):
-    count_X = 0
-    count_XY = 0
-    count_Y = 0 if Y else None
-    
-    for transaction in D:
-        if set(X).issubset(transaction):
-            count_X += 1
-            if Y and set(Y).issubset(transaction):
-                count_XY += 1
-        if Y and set(Y).issubset(transaction):
-            count_Y += 1
-    
-    sup_X = count_X / len(D)
-    sup_XY = count_XY / len(D)
-    sup_Y = count_Y / len(D) if count_Y is not None else None
-    
-    return sup_X, sup_XY, sup_Y
-
-def conf(D, X, Y, count_X, count_XY):
-    return count_XY / count_X if count_X != 0 else 0
-
-def lift(sup_X, sup_Y, sup_XY):
-    return sup_XY / (sup_X * sup_Y) if (sup_X * sup_Y) != 0 else 0
-
-def leverage(sup_X, sup_Y, sup_XY):
-    return sup_XY - (sup_X * sup_Y)
-
-def jaccard(D, X, Y, count_X, count_XY, count_Y):
-    return count_XY / (count_X + count_Y - count_XY) if (count_X + count_Y - count_XY) != 0 else 0
-
-def getRuleMetric(D, X, Y, metric):
-    sup_X, sup_XY, sup_Y = calculate_supports(D, X, Y)
-    count_X = sup_X * len(D)
-    count_XY = sup_XY * len(D)
-    count_Y = sup_Y * len(D) if sup_Y is not None else None
-    
-    metrics = {
-        'sup': sup_XY,
-        'conf': conf(D, X, Y, count_X, count_XY),
-        'lift': lift(sup_X, sup_Y, sup_XY),
-        'leverage': leverage(sup_X, sup_Y, sup_XY),
-        'jaccard': jaccard(D, X, Y, count_X, count_XY, count_Y),
-    }
-    return metrics[metric]
 
 class Recommender:
     """
@@ -83,22 +36,42 @@ class Recommender:
         eclat_recursive(tuple(), item_tidsets, frequent_itemsets)
         return frequent_itemsets
 
-    def createAssociationRules(self, F, minconf, database):
+    def calculate_supports(self, D, X, Y=None):
+        count_X = 0
+        count_XY = 0
+        count_Y = 0 if Y else None
+
+        for transaction in D:
+            if set(X).issubset(transaction):
+                count_X += 1
+                if Y and set(Y).issubset(transaction):
+                    count_XY += 1
+            if Y and set(Y).issubset(transaction):
+                count_Y += 1
+
+        sup_X = count_X / len(D)
+        sup_XY = count_XY / len(D)
+        sup_Y = count_Y / len(D) if count_Y is not None else None
+
+        return sup_X, sup_XY, sup_Y
+
+    def createAssociationRules(self, F, minconf, transactions):
         B = []
         itemset_support = {frozenset(itemset): support for itemset, support in F}
         for itemset, support in F:
             if len(itemset) > 1:
                 for i in range(len(itemset)):
-                    antecedent = frozenset([itemset[i]])
-                    consequent = frozenset(itemset[:i] + itemset[i+1:])
+                    antecedent = frozenset(itemset[:i] + itemset[i+1:])
+                    consequent = frozenset([itemset[i]])
                     antecedent_support = itemset_support.get(antecedent, 0)
                     if antecedent_support > 0:
-                        conf_value = support / antecedent_support
-                        if conf_value >= minconf:
-                            lift_value = getRuleMetric(database, list(antecedent), list(consequent), 'lift')
-                            leverage_value = getRuleMetric(database, list(antecedent), list(consequent), 'leverage')
-                            jaccard_value = getRuleMetric(database, list(antecedent), list(consequent), 'jaccard')
-                            B.append((antecedent, consequent, support, conf_value, lift_value, leverage_value, jaccard_value))
+                        conf = support / antecedent_support
+                        if conf >= minconf:
+                            sup_X, sup_XY, sup_Y = self.calculate_supports(transactions, list(antecedent), list(consequent))
+                            lift_value = sup_XY / (sup_X * sup_Y) if (sup_X * sup_Y) != 0 else 0
+                            leverage_value = sup_XY - (sup_X * sup_Y)
+                            jaccard_value = sup_XY / (sup_X + sup_Y - sup_XY) if (sup_X + sup_Y - sup_XY) != 0 else 0
+                            B.append((antecedent, consequent, support, conf, lift_value, leverage_value, jaccard_value))
         return B
 
     def train(self, prices, database) -> None:
@@ -110,7 +83,7 @@ class Recommender:
         """
         self.prices = prices
         minsup = 0.05  # Using a default minimum support of 5%
-        minconf = 0.2  # Using a default minimum confidence of 70%
+        minconf = 0.3  # Using a default minimum confidence of 70%
         minsup_count = int(minsup * len(database))
         
         print("Calculating frequent itemsets...")
@@ -144,3 +117,10 @@ class Recommender:
         print("Recommendations:", sorted_recommendations)
         
         return [item for item, _ in sorted_recommendations[:max_recommendations]]
+
+# Example usage:
+# recommender = Recommender()
+# prices = [1.0, 2.0, 3.0, 4.0, 5.0]  # Example prices
+# transactions = [[0, 1], [0, 2, 3], [1, 2, 3, 4], [0, 1, 3], [1, 3, 4]]  # Example transactions
+# recommender.train(prices, transactions)
+# print(recommender.get_recommendations([0, 1], max_recommendations=5))
