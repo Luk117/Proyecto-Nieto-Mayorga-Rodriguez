d84998d140f953316cdc183237c458e4e66d0cfa
NataliaMayorga04 <nataliamapr@unisabana.edu.co>
2024-05-21 09:50:06 -0500

    Correcion sup

--- recommender.py
+++ recommender.py
@@ -80,7 +80,7 @@ class Recommender:
     def train(self, prices, database):
         self.database = database
         self.prices = prices
-        minsup_count = int(0.01 * len(database))
+        minsup_count = 0.01
         self.eclat(database, minsup_count)
         self.RULES = self.createAssociationRules(self.frequent_itemsets, minconf=0.1, transactions=self.database)
     
